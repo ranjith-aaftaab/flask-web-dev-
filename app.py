6bff3ea3ddf7d81255d99673e2f4b3159f69f82b
RANJITH-SIST <127429021+ranjith-aaftaab@users.noreply.github.com>
2024-07-14 11:00:27 +0000

    added dynamic data

--- app.py
+++ app.py
@@ -1,11 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template,jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Bengaluru, India',
+        'salary': 'Rs. 10,00,000'
+    },
+    {
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': 'Delhi, India',
+        'salary': 'Rs. 15,00,000'
+    },
+    {
+        'id': 3,
+        'title': 'Front-end Engineer',
+        'location': 'Remote',
+        'salary': 'Rs. 7,00,000'
+    },
+    {
+        'id': 4,
+        'title': 'Backend Engineer',
+        'location': 'Munich, Germany',
+        'salary': '$120,000'
+    }
+]
 
 @app.route('/')
-def hello_world():
-  return 'Hello, Ranjith'
+def hello_ranjith():
+    return render_template('home.html', jobs=JOBS, company_name='Ranjith')
 
-if __name__=='__main__':
-  app.run(host='0.0.0.0', debug=True)
+@app.route('/api/jobs')
+def json_type():
+    return jsonify(JOBS)
+    
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
